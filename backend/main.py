d043986efb1e4b8ec399fdfca96b714894ef691e
Grrom <josejeromelalunio@gmail.com>
2023-01-24 10:47:07 +0800

    setup vue

--- backend/main.py
+++ backend/main.py
@@ -1,21 +1,34 @@
 from flask import Flask
+from flask import request
+
+import json
 
 app = Flask("/")
+
+json_dir = "./jsons/"
 
 
 @app.route("/createEvent")
 def createEvent():
+    with json.load(f'{json_dir}events.json') as f:
+        print(type(f))
+        # json.dump(request.args.get("event_name"), f)
     return "createEvent"
 
 
 @app.route("/listEvent")
 def listEvent():
-    return "listEvent"
+    with open('data.json', 'w') as f:
+        return f
 
 
 @app.route("/updateEvent")
 def updateEvent():
-    return "updateEvent"
+    events = ""
+    with open('data.json', 'w') as f:
+        events = f
+
+    return f
 
 
 @app.route("/deleteEvent")
